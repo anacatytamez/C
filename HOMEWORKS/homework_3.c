4dd38352cc97a73c667c82a1608a930b8969e367
anacatytamez <78567150+anacatytamez@users.noreply.github.com>
2021-03-10 00:36:45 -0600

    Update homework_3.c

--- HOMEWORKS/homework_3.c
+++ HOMEWORKS/homework_3.c
@@ -1,86 +1,92 @@
+/*https://www.youtube.com/watch?v=9xLRPrX42uk*/
 #include <stdio.h>
 
 #define FALSE 0
 #define TRUE 1
 
-int main() 
+int
+main ()
 {
-  int c, numero= FALSE, comillas = FALSE, parentesis = FALSE;/*contadores*/ 
-  while ((c = getchar()) != EOF) 
-  {
-      switch(c)/*sentencia*/
-      {
+  int c, numero = FALSE, comillas = FALSE, parentesis = FALSE;	/*contadores */
+  while ((c = getchar ()) != EOF)
+    {
+      switch (c)		/*sentencia de la variable tipo int*/
+	{
+
+	case '(':
+	  parentesis = TRUE;
+	  putchar (c);
+      break;
       
-      case '[':
-          parentesis = TRUE;
-          putchar(c);
-          break;
-      case '(':
-          parentesis = TRUE;
-          putchar(c);
-          break;
-      case '{':
-          parentesis = TRUE;
-          putchar(c);
-          break;
-      case '<':
-          parentesis = TRUE;
-          putchar(c);
-          break;
-      case ')':
-          parentesis = FALSE;
-          putchar(c);
-          break;
-      case ']':
-          parentesis = FALSE;
-          putchar(c);
-          break;
-      case '}':
-          parentesis = FALSE;
-          putchar(c);
-          break;
-      case '>':
-          parentesis = FALSE;
-          putchar(c);
-          break;
+	case '[':
+	  parentesis = TRUE;
+	  putchar (c);
+
+	case '{':
+	  parentesis = TRUE;
+	  putchar (c);
+
+	case '<':
+	  parentesis = TRUE;
+	  putchar (c);
       
-      default:
-        if(c!='.'&&c!=';'&&c!=':'&&c!='!'&&c!='?'&&c!='-'&&c!='_')
-        {
-            if (parentesis == TRUE||comillas == TRUE) /*me ayudo raul*/
-            {
-            putchar(c);
-            }
-            else
-            {
-                if (comillas == 2)
-                {
-                    comillas = 0;
-                }
-                if (c =='1'||c =='2'||c =='3'||c =='4'||c =='5'||c =='6'||c =='7'||c =='8'||c =='9'||c =='0')
-                {
-                    numero++;
-                }else
-                {
-                  numero = 0;
-                }
-                if (numero == 1)
-                {
-                    putchar('X');
-                }
-                if (numero == 0)
-                {
-                    putchar(c);
-                }
-            }
-        }
+
+	case ')':
+	  parentesis = FALSE;
+	  putchar (c);
+      break;
+      
+	case ']':
+	  parentesis = FALSE;
+	  putchar (c);
+
+	case '}':
+	  parentesis = FALSE;
+	  putchar (c);
+
+	case '>':
+	  parentesis = FALSE;
+	  putchar (c);
+
+
+	default:
+	  if (c != '.' && c != ';' && c != ':' && c != '!' && c != '?')
+	    {
+	      if (parentesis == TRUE || comillas == TRUE)
+		{
+		  putchar (c);
+		}
+	      else
+		{
+		  if (comillas == 2)
+		    {
+		      comillas = 0;
+		    }
+		  if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '0')
+		    {
+		      numero++;
+		    }
+		  else
+		    {
+		      numero = 0;
+		    }
+		  if (numero == 1)
+		    {
+		      putchar ('X');
+		    }
+		  if (numero == 0)
+		    {
+		      putchar (c);
+		    }
+		}
+	    }
 
 
 
 
-        }
+	}
     }
 
 
-    return 0;
-  }
+  return 0;
+}
