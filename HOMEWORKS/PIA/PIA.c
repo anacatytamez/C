7c1efbaeabb1476cb56e621415df8db475879e67
anacatytamez <78567150+anacatytamez@users.noreply.github.com>
2021-05-20 13:37:54 -0500

    Update PIA.c

--- HOMEWORKS/PIA/PIA.c
+++ HOMEWORKS/PIA/PIA.c
@@ -10,35 +10,33 @@ char Snom[20];
  int c;
 } 
  
- 
-
-      nom,Inv[MAX];
+nom,Inv[MAX];
 
 void menu1(void)
 {
-    
        printf("\n");
        printf("\n");
        printf("Ingrese el producto (6 caracteres):\n");
-     scanf(" %[^\n]",nom.Snom);
+     scanf(" %[^\n]", nom.Snom);
        printf("Cantidad de producto (unidades, no por kilos):\n");
-     scanf("%d",&nom.cn);
+     scanf("%d", &nom.cn);
        printf("Valor del producto individual:\n");
-     scanf("%d",&nom.c);
+     scanf("%d", &nom.c);
        
 
 
-for(char i=0; i<MAX; i++){
-    if(Inv[i].cd==0){
+for (char i = 0; i < MAX; i++){
+    if (Inv[i].cd == 0){
          
-       Inv[i]=nom;
-       Inv[i].cd=i+1;
+       Inv[i] = nom;
+       Inv[i].cd = i+1;
          
-       printf(" Codigo generado de manera automatica, es: %i \n",i+1);
-       i=MAX;}
+       printf (" Codigo generado de manera automatica, es: %i \n", i+1);
+       i  =MAX;
+    }
      
-else if (i==MAX-1)
-printf("Registro  lleno\n");}}
+else if (i == MAX -1)
+printf ("Registro  lleno\n");}}
 
         
 
@@ -46,24 +44,26 @@ void menu2(void){
      
    char proimp=0;
     printf("\n");
-     printf("\n");
-      printf("Ingrese el codigo del producto a imprimir:\n");
-       scanf("%d",&proimp);
+    printf("\n");
+    printf("Ingrese el codigo del producto a imprimir:\n");
+    scanf("%d", &proimp);
    
-if(proimp>0 && proimp<=10){
-   proimp--;
-   printf(" |--| Codigo |--| Nombre |--| Cantidad |--| Costo |--| \n");
-   printf(" |--| 0000%d  |--| %s |--|   %2d     |--|  %2d   |--|\n", Inv[proimp].cd, Inv[proimp].Snom, Inv[proimp].cn, Inv[proimp].c);
-   }else 
-       printf("El registro no existe\n");
+if(proimp > 0 && proimp <= 10){
+   proimp --;
+   printf (" |--| Codigo |--| Nombre |--| Cantidad |--| Costo |--| \n");
+   printf (" |--| 0000%d  |--| %s |--|   %2d     |--|  %2d   |--|\n", Inv[proimp].cd, Inv[proimp].Snom, Inv[proimp].cn, Inv[proimp].c);
+   }
+else 
+   printf("El registro no existe\n");
 }
 
 void menu3(void)
 {
    printf("\n");
    printf("\n");
-    printf("|--| Codigo |--| Nombre |--| Cantidad |--| Costo |--|\n");
-      for(int i=0;i<MAX;i++){
+   printf("|--| Codigo |--| Nombre |--| Cantidad |--| Costo |--|\n");
+
+for (int i = 0; i < MAX; i++){
         
         printf("|--| 0000%d  |--| %s |--|   %2d     |--|  %2d   |--|\n", Inv[i].cd, Inv[i].Snom, Inv[i].cn, Inv[i].c);
 		 }
@@ -75,16 +75,16 @@ void menu4(void)
      float Valor;
     
     printf("\n");
-     printf("\n");
-      printf("Ingrese el codigo del producto a imprimir su valor de inventario\n");
-       scanf("%d", &vainv);
+    printf("\n");
+    printf("Ingrese el codigo del producto a imprimir su valor de inventario\n");
+    scanf("%d", &vainv);
      
-     if(vainv>=0 && vainv<=10){
+     if (vainv >= 0 && vainv <= 10){
    
     vainv--;
     
      Valor = Inv[vainv].c*Inv[vainv].cn;
-       printf("El valor de inventario de dicho producto %d es %6.2f \n",vainv+1, Valor); }
+     printf("El valor de inventario de dicho producto %d es %6.2f \n",vainv+1, Valor); }
        
        else
         printf("El registro no existe\n");}
@@ -94,11 +94,11 @@ void menu5(void){
     
     printf("\n");
      printf("\n");
-      for(int i=0;i<MAX;i++){
+      for(int i = 0; i < MAX; i++){
               
         float vIn;
          vIn = Inv[i].c*Inv[i].cn;
-           printf("Estos son los valores de inventario de tus productos %d es %6.2f \n",Inv[i].cd ,vIn);}
+          printf("Estos son los valores de inventario de tus productos %d es %6.2f \n",Inv[i].cd ,vIn);}
            
 }
 
@@ -112,19 +112,19 @@ main ()
             
             
             printf(" ----DULCES DETALLES---- \n");
-             printf("\n");
+            printf("\n");
             printf(" -------MENU------- \n");
-             printf("\n");            
+            printf("\n");            
             printf("Para ingresar un producto pulse [1]\n");
-             printf("\n");
+            printf("\n");
             printf("Para imprimir un producto pulse [2]\n");
-             printf("\n");
+            printf("\n");
             printf("Para imprimir todos los productos pulse [3]\n");
-             printf("\n");
+            printf("\n");
             printf("Para imprimir el valor de inventario de un producto pulse[4]\n");
-             printf("\n");
+            printf("\n");
             printf("Para imprimir el valor del inventario total pulse [5]\n");
-             printf("\n");
+            printf("\n");
             printf("¿Quiere salirse?, pulse [6]\n");
             scanf( "%d", &ch );
             
